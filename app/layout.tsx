250965351e3a772f99ea07a1dcf4e070c5a9cce8
MohamedH1000 <mohammedhisham115@gmail.com>
2024-08-22 17:40:30 +0300

    change metatags

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ابو خالد /تسديد قروض",
+  title: "0546742134 ابو خالد /تسديد قروض",
   description: "سداد قروض",
 };
 
